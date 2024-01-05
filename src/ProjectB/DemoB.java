759b832bcbaea74a5cbb020e68de758612d4c997
vfleitasoria <155499181+vfleitasoria@users.noreply.github.com>
2024-01-05 18:24:35 -0500

    Update DemoB.java

--- src/ProjectB/DemoB.java
+++ src/ProjectB/DemoB.java
@@ -15,3 +15,7 @@ public static void main(String[]args) {
 		System.out.println("Git project pull...");
 System.out.println("Comming from gitHub");
 }
+System.out.println("Comming from gitHub");
+}
+
+}
