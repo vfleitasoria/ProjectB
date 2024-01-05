abf21d4e96b14b080694d115c6234f4c98121c63
vflei <vflei@Vladimir.attlocal.net>
2024-01-04 20:27:35 -0500

    Initial Commit

--- src/ProjectB/DemoB.java
+++ src/ProjectB/DemoB.java
@@ -2,4 +2,12 @@ package ProjectB;
 
 public class DemoB {
 
+public static void main(String[]args) {
+		
+		System.out.println("Monday");
+		
+		System.out.println("Git project push...");
+		System.out.println("Git project pull...");
+		
+	}
 }
